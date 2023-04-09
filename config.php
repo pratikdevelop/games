b6e065c749b11bb5f1212ace4e61d441349ee2f2
Pratik_5678 <pratikraut88895@gmail.com>
2023-04-09 17:08:44 +0530

    new commit\

--- config.php
+++ config.php
@@ -37,23 +37,25 @@ try {
             $pdo->exec($statement);
         }
 
-        // $games = [
-        //     [
-        //         "name"=> "Planet destroy",  "image"=>"images/planet_destroy.jpg", "url"=>"planet_destroy/index.html"],
-        //        [ "name"=> "Ninja game",  "image"=>"images/menja.jpg", "url"=>"min_game/index.html"],
-        //        [ "name"=> "Snake game",  "image"=>"images/snake_game.jpg", "url"=>"snake_game/index.html"],
-        //        [ "name"=> "Typing master",  "image"=>"images/typing.jpg", "url"=>"typing_master/index.html"],
-        //        [ "name"=> "Photo editing",  "image"=>"images/photo_editing.jpg", "url"=>"photo_editing/index.html"],
-        //        [ "name"=> "Memory game",  "image"=>"images/memory.jpg", "url"=>"memory/index.html"],
-        //        [ "name"=> "Color blast",  "image"=>"images/color_blast.jpg", "url"=>"color_blast/index.html"],
-        //        [ "name"=> "Tic Tac Toe",  "image"=>"images/tic.jpg", "url"=>"tic-tac_toe/index.html"],
-        //        [ "name"=> "Word scramble ",  "image"=>"images/word_scramble.jpg", "url"=>"word_scramble/index.html"],
-        //     ];
-        // foreach ($games as $key => $value) {
-        //     $sql = "INSERT INTO games (game_name , game_image, url) VALUES (?,?,?)";
-        //     $stmt= $pdo->prepare($sql);
-        //     $stmt->execute([$value['name'], $value['image'], $value['url']]);
-        // }
+        $games = [
+            [
+                "name"=> "Planet destroy",  "image"=>"images/planet_destroy.jpg", "url"=>"planet_destroy/index.html"],
+               [ "name"=> "Ninja game",  "image"=>"images/menja.jpg", "url"=>"min_game/index.html"],
+               [ "name"=> "Snake game",  "image"=>"images/snake_game.jpg", "url"=>"snake_game/index.html"],
+               [ "name"=> "Typing master",  "image"=>"images/typing.jpg", "url"=>"typing_master/index.html"],
+               [ "name"=> "Photo editing",  "image"=>"images/photo_editing.jpg", "url"=>"photo_editing/index.html"],
+               [ "name"=> "Memory game",  "image"=>"images/memory.jpg", "url"=>"memory/index.html"],
+               [ "name"=> "Color blast",  "image"=>"images/color_blast.jpg", "url"=>"color_blast/index.html"],
+               [ "name"=> "Tic Tac Toe",  "image"=>"images/tic.jpg", "url"=>"tic-tac_toe/index.html"],
+               [ "name"=> "Word scramble ",  "image"=>"images/word_scramble.jpg", "url"=>"word_scramble/index.html"],
+                ["name"=>"Drawing ", "image"=>"images/drawing.jpg", "url"=>"drawing/index.html"],
+                ["name"=>"Word Guessing ", "image"=>"images/guess_word.jpg", "url"=>"drawing/index.html"]
+            ];
+        foreach ($games as $key => $value) {
+            $sql = "INSERT INTO games (game_name , game_image, url) VALUES (?,?,?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$value['name'], $value['image'], $value['url']]);
+        }
         return $pdo;
     }
 
